658890d620faeb605494d3105e5fbbfd9808159d
Yao Marie Joelle <yaomariejoelle209@gmail.com>
2026-05-25 11:46:12 +0000

    ajout de l'instance CheckingAccount

--- src/BankManager.java
+++ src/BankManager.java
@@ -1,3 +1,11 @@
 //classe principale
 public class BankManager {
+    public static void main (String[] args){
+        //instance de checkingAccount
+        CheckingAccount checking = new CheckingAccount();
+        checking.account= "CHK001";
+        checking.balance=  500000.00;
+        checking.limit= 1000000.00;
+
+    }
 }
